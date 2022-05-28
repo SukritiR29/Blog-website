36586aa734ec688cabbb709e488d3b46cd69afbe
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2022-05-28 23:29:25 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,12 @@
 import { useState, useEffect } from "react";
 import BlogList from "./bloglist";
-const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+import useFetch from "./usefetch";
 
+const Home = () => {
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
  
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(res => {
-        if (!res.ok) { //error coming back from server
-        throw Error('could not fetch the data for that resourece');
-      }
-      return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-        setError(null);
-      })
-      .catch(err => {
-        //auto catches networks / connetion error
-        setIsPending(false);
-        setError(err.message);
-      })
-    }, 1000);
-  }, []);
+ 
 
   return (
     <div className="home">
