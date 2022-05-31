50f298c8b8a6c33a8efe9f9b609d23f0d50b7d57
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2022-05-31 10:13:13 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,9 +5,6 @@ import useFetch from "./usefetch";
 const Home = () => {
   const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
  
-
- 
-
   return (
     <div className="home">
       { error && <div>{ error }</div>}
